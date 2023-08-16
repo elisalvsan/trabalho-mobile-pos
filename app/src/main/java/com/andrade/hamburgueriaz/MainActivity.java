dfd9a4b9b19033658cc6fbb20252950a66f8a495
Elisa Andrade <elisalvsan@gmail.com>
2023-08-16 00:56:58 -0300

    Primeira entrega - Aula pratica 1

---
+++ app/src/main/java/com/andrade/hamburgueriaz/MainActivity.java
@@ -0,0 +1,28 @@
+package com.andrade.hamburgueriaz;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        EditText name = findViewById(R.id.nomeCliente);
+        name.setAutofillHints(View.AUTOFILL_HINT_NAME);
+
+
+    }
+}
