38c7c70269c17e048d20fcfdb21e972ed8e2dc1f
Elisa Andrade <elisalvsan@gmail.com>
2023-08-16 03:43:03 -0300

    Aula pratica 3 - Etapa 1

--- app/src/main/java/com/andrade/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/andrade/hamburgueriaz/MainActivity.java
@@ -7,9 +7,13 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numero=0;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -23,6 +27,27 @@ public class MainActivity extends AppCompatActivity {
         EditText name = findViewById(R.id.nomeCliente);
         name.setAutofillHints(View.AUTOFILL_HINT_NAME);
 
+        TextView quantidade = (TextView) findViewById(R.id.valorQuantidade);
 
+        Button adicionarQuantidade = findViewById(R.id.adicionarQuantidade);
+        adicionarQuantidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numero++;
+                quantidade.setText(String.valueOf(numero));
+            }
+        });
+
+        Button subtrairQuantidade = findViewById(R.id.subtrairQuantidade);
+        subtrairQuantidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(numero>0){
+                    numero--;
+                    quantidade.setText(String.valueOf(numero));
+                }
+
+            }
+        });
     }
 }
